979dfee93b941fe31929b738e53c9c1219956a9d
Valeria Monge <Valeria Monge@DESKTOP-KGOH2QL>
2018-11-12 00:04:45 -0600

    initial prj

---
+++ paw.tarea2.main/Logica/2Service/Servicio1.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace paw.tarea2.main.Logica
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Servicio1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Servicio1.svc or Servicio1.svc.cs at the Solution Explorer and start debugging.
+    public class Servicio1 : IServicio1
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
